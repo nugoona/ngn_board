fe86c15b7e414fbed08b2ea665fe3f5d20ae3910
Oscar <oscar@nugoona.com>
2025-07-24 21:31:22 +0900

    add debug logs to showLoading/hideLoading functions

--- ngn_wep/dashboard/static/js/common.js
+++ ngn_wep/dashboard/static/js/common.js
@@ -1,15 +1,19 @@
 // File: static/js/common.js
 
 function showLoading(target) {
+  console.log("[DEBUG] showLoading 호출됨:", target);
   $(target).css({ display: "flex" });
   $(target).closest(".table-wrapper, .performance-summary-wrapper").addClass("loading");
+  console.log("[DEBUG] showLoading 완료:", target);
 }
 
 function hideLoading(target) {
+  console.log("[DEBUG] hideLoading 호출됨:", target);
   $(target).css({ display: "none" });
   $(target).closest(".table-wrapper, .performance-summary-wrapper").removeClass("loading");
   // 추가: 스피너와 로딩 텍스트도 완전히 숨김
   $(target).find(".spinner, .loading-text").hide();
+  console.log("[DEBUG] hideLoading 완료:", target);
 }
 
 function getRequestData(page = 1, extra = {}) {
